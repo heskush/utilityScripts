55928bc06ebbb4f7bd7158c1f60146905780951e
Hemant Kumar <hemant.kumar@mobileum.com>
2018-01-27 21:34:29 +0530

    Made changes to startWork script

--- startWork.sh
+++ startWork.sh
@@ -1,6 +1,6 @@
-start-dfs.sh;
-start-yarn.sh;
-mr-jobhistory-daemon.sh start historyserver;
-oozied.sh start;
-sh $SPARK_HOME/sbin/start-history-server.sh;
+start-dfs.sh; #3 JPS
+start-yarn.sh; #2 JPS 
+mr-jobhistory-daemon.sh start historyserver; # 1 JPS
+oozied.sh start; # 
+sh $SPARK_HOME/sbin/start-history-server.sh; # 1 JPS
 start-hbase.sh
